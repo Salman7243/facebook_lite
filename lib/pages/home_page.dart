2d7dcc235dfa7ab7acf49a760cd08e676344ba97
Salman7243 <98895288+Salman7243@users.noreply.github.com>
2023-02-12 23:07:44 +0400

    Add files via upload
    today

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:facebook_lite/widgets/home/menubar.dart';
+import 'package:facebook_lite/widgets/home/post.dart';
+import 'package:facebook_lite/widgets/home/postbar.dart';
+import 'package:facebook_lite/widgets/home/storybar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 8.0),
+      child: ListView(
+        children: [
+          Postbar(),
+          Divider(thickness: 1, color: Colors.black12),
+          Menubar(),
+          Divider(thickness: 1, color: Colors.black12),
+          Storybar(),
+          Divider(thickness: 1, color: Colors.black12),
+          Post(),
+        ],
+      ),
+    );
+  }
+}
